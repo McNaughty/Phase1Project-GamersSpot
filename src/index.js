b036c491e823af6ef174974a93a4f84b96d0e35d
Winnie <wimacoj@gmail.com>
2023-11-03 13:26:31 +0300

    Changes to the listing table

--- src/index.js
+++ src/index.js
@@ -9,38 +9,6 @@ $(document).ready(function () {
   });
 });
 
-// fetch game data
-
-// fetch("http://localhost:3000/games")
-//   .then(function (response) {
-//     return response.json();
-//   })
-//   .then(function (gameData) {
-//     let placeholder = document.querySelector("#data-output");
-//     let output = "";
-//     // console.log(gameData);
-//     window.gameList = gameData;
-//     for (let game of gameData) {
-//       output += `
-//         <tr>
-//         <td>${game.ID}</td>
-//         <td><img src='${game.Poster}'></td>
-//         <td>${game.Title}</td>
-//         <td>${game.Genre}</td>
-//         <td>${game.Publisher}</td>
-//         <td>${game.Status}</td>
-//         <td>${game.Steamurl}</td>
-
-//         </tr>
-
-//         `;
-
-//         //colUniqueVal(gameData);
-//     }
-
-//     placeholder.innerHTML = output;
-//   });
-
 // Get unique values for the columns being filtered
 
 function colUniqueVal() {
@@ -79,58 +47,40 @@ function colUniqueVal() {
           unique_col_val_obj[column_index] = new Array(storedContent);
         }
       }
-
-      //   //  Check if the column index already exists in our col_val_obj object
-      //     if (column_index in unique_col_val_obj){
-      //         //check if the cell value exists
-      //         if (unique_col_val_obj[column_index].includes(storedContent)){
-      //             alert(rowContent + "already exists :" + unique_col_val_obj[column_index])
-      //         }else{
-      //             unique_col_val_obj[column_index].push(storedContent)
-      //             alert("Array after adding the row content:" + unique_col_val_obj[column_index])
-      //         }
-      //     } else{
-      //         unique_col_val_obj[column_index] = new Array(storedContent)
-      //     }
     });
   });
 
-//   for (i in unique_col_val_obj) {
-//     alert( "column index:" + i + "has unique values: \n " + unique_col_val_obj[i]
-//     );
-//   }
   updateSelectOptions(unique_col_val_obj);
 }
 
 //Function to add the unique values to the <option> tags for the drop down
 
-function updateSelectOptions(unique_col_val_obj){
-    allFilters = document.querySelectorAll(".tableFilter")
-    allFilters.forEach((filterE) => {
-       let colmn_index = filterE.parentElement.getAttribute("col-index");
-        console.log(unique_col_val_obj[colmn_index]);
- 
-        unique_col_val_obj[colmn_index].forEach((i) => {
-            
-        filterE.innerHTML = filterE.innerHTML + `\n<option value="${i}">${i}</option>`
-       })
-    })
+function updateSelectOptions(unique_col_val_obj) {
+  allFilters = document.querySelectorAll(".tableFilter");
+  allFilters.forEach((filterE) => {
+    let colmn_index = filterE.parentElement.getAttribute("col-index");
+    console.log(unique_col_val_obj[colmn_index]);
 
-   // filter_rows();
+    unique_col_val_obj[colmn_index].forEach((i) => {
+      filterE.innerHTML =
+        filterE.innerHTML + `\n<option value="${i}">${i}</option>`;
+    });
+  });
+
+  // filter_rows();
 }
 
-function filter_rows(){
-  allFilters = document.querySelectorAll(".tableFilter")
-  let filter_value_object = {}
+function filter_rows() {
+  allFilters = document.querySelectorAll(".tableFilter");
+  let filter_value_object = {};
 
   allFilters.forEach((filterElem) => {
     let column_indexx = filterElem.parentElement.getAttribute("col-index");
 
-    value = filterElem.value
-    console.log (value);
-    if (value != "all"){
+    value = filterElem.value;
+    console.log(value);
+    if (value != "all") {
       filter_value_object[column_indexx] = value;
-      
     }
   });
 
@@ -142,40 +92,35 @@ function filter_rows(){
 
     allFilters.forEach((filter_row) => {
       column_indexx = filter_row.parentElement.getAttribute("col-index");
-      column_cell_value_dictionary[column_indexx] = row.querySelector("td:nth-child(" + column_indexx + ")"
+      column_cell_value_dictionary[column_indexx] = row.querySelector(
+        "td:nth-child(" + column_indexx + ")"
       ).innerHTML;
 
       //console.log(column_cell_value_dictionary[column_indexx]);
-    })
+    });
 
-    for (let col_i in filter_value_object ){
+    for (let col_i in filter_value_object) {
       filter_value = filter_value_object[col_i];
       console.log(filter_value);
-      
-      rowCellValue = column_cell_value_dictionary[col_i]
 
-      if (rowCellValue.indexOf(filter_value) == -1 && filter_value != "all"){
+      rowCellValue = column_cell_value_dictionary[col_i];
+
+      if (rowCellValue.indexOf(filter_value) == -1 && filter_value != "all") {
         displayRow = false;
         break;
       }
     }
 
-    if (displayRow == true){
-      row.style.display = "table-row"
-    }else{
-      row.style.display = "none"
+    if (displayRow == true) {
+      row.style.display = "table-row";
+    } else {
+      row.style.display = "none";
     }
-
-  })
-
-
+  });
 }
 
-
-
-
 document.addEventListener("DOMContentLoaded", () => {
-  fetch("https://retoolapi.dev/xRKZpW/games")
+  fetch("https://retoolapi.dev/iN1KN2/games")
     .then(function (response) {
       return response.json();
     })
@@ -194,7 +139,6 @@ document.addEventListener("DOMContentLoaded", () => {
         <td>${game.Publisher}</td>
         <td>${game.Status}</td>
         <td>${game.Steamurl}</td>
-        
         </tr>
         
         `;
@@ -203,9 +147,7 @@ document.addEventListener("DOMContentLoaded", () => {
       }
 
       placeholder.innerHTML = output;
-      
-      colUniqueVal();   
-      //filter_rows();
-    });
 
+      colUniqueVal();
+    });
 });
