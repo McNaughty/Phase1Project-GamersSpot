f5222ddbf6c3f1732b945407f05fd72f5675d7c4
Winnie <wimacoj@gmail.com>
2023-11-03 15:24:24 +0300

    Changes to the userreg api

--- src/Registration.js
+++ src/Registration.js
@@ -1,92 +1,63 @@
-// function test() {
-//   //let selectedGender = "Prefer not to say"
-//   userData = {};
+function test() {
+  userData = {};
 
-//   let fName = document.getElementById("fName").value;
-//   let emailAd = document.getElementById("emailAd").value;
-//   let bornDay = document.getElementById("bDay").value;
-//   let streetAd = document.getElementById("streetAd").value;
-//   let streetAd2 = document.getElementById("streetAd2").value;
-//   let city = document.getElementById("city").value;
-//   let region = document.getElementById("region").value;
-//   let postalCode = document.getElementById("pcode").value;
+  let fName = document.getElementById("fName").value;
+  let emailAd = document.getElementById("emailAd").value;
+  let bornDay = document.getElementById("bDay").value;
+  let streetAd = document.getElementById("streetAd").value;
+  let psnusername = document.getElementById("psn").value;
+  let city = document.getElementById("city").value;
+  // let region = document.getElementById("region").value;
+  let postalCode = document.getElementById("pcode").value;
 
-//   let selectElement = document.getElementById("country");
-//   let countryOutput = selectElement.value;
+  let selectElement = document.getElementById("country");
+  let countryOutput = selectElement.value;
 
-//   userData.FullName = fName;
-//   userData.Email = emailAd;
-//   userData.Birthdate = bornDay;
-//   let getSelectedValue = document.querySelector('input[name="gender"]:checked');
-//   if (getSelectedValue != null) {
-//     let selectedGender = getSelectedValue.value;
-//     userData.Gender = selectedGender;
-//   }
-//   userData.StreetAddress = streetAd;
-//   userData.StreetAddressTwo = streetAd2;
-//   userData.Country = countryOutput;
-//   userData.City = city;
-//   userData.Region = region;
-//   userData.PostalCode = postalCode;
+  userData.FullName = fName;
+  userData.Email = emailAd;
+  userData.Birthdate = bornDay;
+  let getSelectedValue = document.querySelector('input[name="gender"]:checked');
+  if (getSelectedValue != null) {
+    let selectedGender = getSelectedValue.value;
+    userData.Gender = selectedGender;
+  }
+  userData.StreetAddress = streetAd;
+  userData.psnaccount = psnusername;
+  userData.Country = countryOutput;
+  userData.City = city;
+  // userData.Region = region;
+  userData.PostalCode = postalCode;
 
-//   //console.log(userData);
+  //console.log(userData);
 
-//   regUser(userData);
-// }
+  regUser(userData);
+}
 
-// function regUser(userData) {
-//   //console.log(userData);
-//   //jsonData = { ...userData };
+function regUser(userData) {
+  //console.log(userData);
+  jsonData = { ...userData };
 
-// //   newJsonD = JSON.stringify(jsonData);
-// //   //userReg = JSON.parse({newJsonD});
-// //   console.log(JSON.stringify(jsonData));
+  fetch("https://retoolapi.dev/qu2Ysx/userdata", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(jsonData),
+  })
+    .then((response) => response.json())
+    .then((registeredUser) =>
+      console.log("newUser registration", registeredUser)
+    )
+    .catch((err) => console.log(err));
+  //console.log(jsonData);
+}
 
-//   fetch("http://localhost:3000/users/", {
-
-//     method: "POST",
-//     headers: {
-//       "Content-Type": "application/json",
-//     },
-//     body: jsonData
-//   })
-//     .then((response) => response.json())
-//     .then((registeredUser) => console.log("newUser registration", registeredUser))
-//     .catch((err) => console.log(err));
-//   //console.log(jsonData);
-// }
-
-//document.addEventListener("DOMContentLoaded", () => {
-  //   document.getElementById("reguser").addEventListener("click", function (e) {
-  //     e.preventDefault();
-
-  //     test();
-
-  //     // regUser(userData);
-  //   });
-
-  let form = document.querySelector(".form");
-  form.addEventListener("submit", handleSubmit);
-
-  function handleSubmit(e) {
+document.addEventListener("DOMContentLoaded", () => {
+  document.getElementById("reguser").addEventListener("click", function (e) {
     e.preventDefault();
 
-    let formData = new FormData(form);
-    let data = Object.fromEntries(formData);
-    jsonData = JSON.stringify(data);
+    test();
 
-    fetch("http://localhost:3000/users", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: jsonData,
-    })
-    //   .then((response) => response.json())
-    //   .then((registeredUser) => registeredUser)
-    //     //console.log("newUser registration", registeredUser)
-    //   )
-    //   .catch((err) => console.log(err));
-    // console.log(jsonData);
-  }
-//});
+    // regUser(userData);
+  });
+});
